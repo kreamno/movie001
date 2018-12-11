f0906a8dd0e53aea97eb7db1e39041d224d25787
yujeong <kreamno@gmail.com>
2018-12-11 21:05:12 +0900

    Update movie.c

--- movie.c
+++ movie.c
@@ -16,6 +16,14 @@ void* mv_genMvInfo(char* name, float score, int runTime, char* country)
 	movInfo_t* mvPtr;
 	
 	//allocate memory and set the member variables
+	mvPtr = (movInfo_t*)malloc(500*sizeof(movInfo_t));
+	
+	mvPtr.name = name;
+	mvPtr.score = score;
+	mvPtr.runTime = runTime;
+	mvPtr.madeIn = country;
+	
+	free(mvPtr);
 	
 	return (void*)mvPtr;
 }
@@ -36,22 +44,53 @@ void printMv(void* obj)
 
 int mv_printAll(void* obj, void* arg)
 {
-	
+	movInfo_t* mvPtr = (movInfo_t*)obj;
+	movInfo_t* printMv = (movInfo_t*)arg;
 }
 
 int mv_printScore(void* obj, void* arg)
 {
+	movInfo_t* mvPtr = (movInfo_t*)obj;
+	movInfo_t* printMv = (movInfo_t*)arg;
 	
+	if (mvPtr == NULL)
+	{
+		printf("[ERROR] failed to print the movie Info! (object is NULL)\n");
+	}
+	
+	printf("Name : %s, score : %f\n", mvPtr->name, mvPtr->score);
+	
+	return 0;
 }
 
 int mv_printRunTime(void* obj, void* arg)
 {
+	movInfo_t* mvPtr = (movInfo_t*)obj;
+	movInfo_t* printMv = (movInfo_t*)arg;
 	
+	if (mvPtr == NULL)
+	{
+		printf("[ERROR] failed to print the movie Info! (object is NULL)\n");
+	}
+	
+	printf("Name : %s, RunTime : %f\n", mvPtr->name, mvPtr->runTime);
+	
+	return 0;
 }
 
 int mv_printCountry(void* obj, void* arg)
 {
+	movInfo_t* mvPtr = (movInfo_t*)obj;
+	movInfo_t* printMv = (movInfo_t*)arg;
 	
+	if (mvPtr == NULL)
+	{
+		printf("[ERROR] failed to print the movie Info! (object is NULL)\n");
+	}
+	
+	printf("Name : %s (%s)", mvPtr->name, mvPtr->madeIn);
+	
+	return 0;	
 }
 
 
